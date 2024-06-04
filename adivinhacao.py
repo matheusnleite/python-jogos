358d5102f56735334c9bc04a657b672b7105ed68
Matheus <matheusn06leite@gmail.com>
2024-06-04 11:17:08 -0300

    melhoria no jogo de adivinhação

--- adivinhacao.py
+++ adivinhacao.py
@@ -10,7 +10,17 @@ print("Voce digitou o numero ", chute)
 
 chute = int(chute)
 
-if(numero_secreto == chute):
+acertou = chute == numero_secreto
+maior = chute > numero_secreto
+menor = chute < numero_secreto
+
+if(acertou):
     print("Voce acertou!")
 else:
-    print("Voce errou!")
+    if(maior):
+        print("Voce errou! O seu chute foi maior do que o numero secreto")
+    elif(menor):
+        print("Voce errou! O seu chute foi menor do que o numero secreto")
+
+
+print("Fim do jogo")
