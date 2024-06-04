2d824ad5d778b2ac14a392d37cf815f6eb401a1d
Matheus <matheusn06leite@gmail.com>
2024-06-04 11:29:59 -0300

    Implementação de loop no jogo de adivinhação

--- adivinhacao.py
+++ adivinhacao.py
@@ -3,24 +3,27 @@ print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************")
 
 numero_secreto = 42
+total_de_tentativas = 3
+rodada = 1
 
-chute = input("Digite o seu numero: ")
+while(rodada <=2 total_de_tentativas):
+    print("Tentativa", rodada,"de", total_de_tentativas)
+    chute = input("Digite o seu numero: ")
+    print("Voce digitou o numero ", chute)
+    chute = int(chute)
 
-print("Voce digitou o numero ", chute)
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
 
-chute = int(chute)
-
-acertou = chute == numero_secreto
-maior = chute > numero_secreto
-menor = chute < numero_secreto
-
-if(acertou):
-    print("Voce acertou!")
-else:
-    if(maior):
-        print("Voce errou! O seu chute foi maior do que o numero secreto")
-    elif(menor):
-        print("Voce errou! O seu chute foi menor do que o numero secreto")
-
+    if(acertou):
+        print("Voce acertou!")
+    else:
+        if(maior):
+            print("Voce errou! O seu chute foi maior do que o numero secreto")
+        elif(menor):
+            print("Voce errou! O seu chute foi menor do que o numero secreto")
+  
+    rodada = rodada + 1
 
 print("Fim do jogo")
