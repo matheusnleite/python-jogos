a08451300fe421c63d9a32604ba481c5ff1b8b30
Matheus <matheusn06leite@gmail.com>
2024-06-04 11:42:50 -0300

    Substituição do loop while pelo for

--- adivinhacao.py
+++ adivinhacao.py
@@ -4,9 +4,9 @@ print("*********************************")
 
 numero_secreto = 42
 total_de_tentativas = 3
-rodada = 1
 
-while(rodada <= total_de_tentativas):
+
+for rodada in range(1, total_de_tentativas + 1): #range é para definir o intervalo que o loop for vai rodar através da variavel rodada, por isso não é necessário definir um valor para ela antes, pois ela assumirá o primeiro valor do range. É necessario colocar o +1 apos a variavel pois ele o ultimo valor é exclusivo, ou seja, a variavel rodada nao assumirá o último valor.
     print("Tentativa {} de {}".format(rodada, total_de_tentativas)) #substituir valores na string, onde tem {} entra o valor das variáveis, isso se chama "String interpolation"
     chute = input("Digite o seu numero: ")
     print("Voce digitou o numero ", chute)
@@ -23,7 +23,5 @@ while(rodada <= total_de_tentativas):
             print("Voce errou! O seu chute foi maior do que o numero secreto")
         elif(menor):
             print("Voce errou! O seu chute foi menor do que o numero secreto")
-  
-    rodada = rodada + 1
 
 print("Fim do jogo")
