a68d5b2e8a4d67f6688e7ab6d5027ad1978bdda1
Matheus <matheusn06leite@gmail.com>
2024-07-11 11:33:01 -0300

    melhorando o jogo de forca adicionando a quantidade de letras da palavra usando listas

--- forca.py
+++ forca.py
@@ -2,22 +2,36 @@ def jogar(): #definindo a funcao para chama-la no outro codigo
     print("*********************************")
     print("***Bem vindo ao jogo de Forca!***")
     print("*********************************")
-    palavra_secreta = "banana"
+    palavra_secreta = "banana".upper()
+    letras_acertadas = ["_","_","_","_","_","_"]
+
     enforcou = False
     acertou = False
+    erros = 0
+
+    print(letras_acertadas)
 
     while(not enforcou and not acertou):  #not é para negar a variavel
         chute = input("Qual letra? ")
-        chute = chute.strip()
+        chute = chute.strip().upper()
 
-        index=0
-        for letra in palavra_secreta:
-            if(chute.upper() == letra.upper()):
-                print("Encontrei a letra {} na posição {}".format(chute, index))
-            index = index+1
-        print("jogando...")
+
+        if(chute in palavra_secreta):
+            index=0
+            for letra in palavra_secreta:
+                if(chute == letra):
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
+        print(letras_acertadas)
     
-
+    if(acertou):
+        print("Voce ganhou!")
+    else:
+        print("Voce perdeu!")
     print("Fim do jogo")
     
 if(__name__ == "__main__"): #se o codigo está sendo executado diretamente (sem ser chamado como função), o python seta o valor __main__ para a variavel __name__, então esse if é para executar a funcao jogar automaticamente somente se o codigo foi executado diretamente
